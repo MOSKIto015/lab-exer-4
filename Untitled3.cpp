4afff71d1ce5e00762a5163c8c7e1f04f77a1785
MOSKIto015 <46777489+MOSKIto015@users.noreply.github.com>
2019-01-17 17:15:43 +0800

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int num[5];
+int* ptr = num;
+
+cout << "Enter 5 numbers: " << endl;
+cin >> num[0] >> num[1] >> num[2] >> num[3] >> num[4];
+cout << "Your inputs are: " << endl;
+for(int x = 0;x < 5;x++)
+{
+
+cout << *ptr << endl;
+ptr++;
+}
+}
